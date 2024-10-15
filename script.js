c026665d0364de4eb66c540cbd7424d2c6b91191
ASSabbir <atiksabbir1125@gmail.com>
2024-10-16 02:46:37 +0600

    added some section

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-const locomotive = () => {
-    gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("#main"),
-        smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
-
-
-
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
-
-}
 const curser=()=>{
-    console.log('jkhnkjh')
+   
     document.querySelector('.page1').addEventListener('mousemove', (e) => {
         gsap.to('#curser', {
             opacity: 1,
@@ -82,7 +49,6 @@ const curser=()=>{
         
     })
 }
-
 const navberDesign = () => {
     document.querySelector('#menu').addEventListener('click', () => {
 
@@ -146,31 +112,31 @@ const navberDesign = () => {
 
     })
 }
-const page2=()=>{
-    gsap.to('.page2 hr',{
+const page2=(page,textBlack,TomorrowBrand)=>{
+    gsap.to(`${page} hr`,{
         width:'100%',
         duration:2,
         scrollTrigger:{
-            trigger:'#text-black',
+            trigger:textBlack,
             scrub:1,
             start:'top 80%',
         }
     })
-    gsap.from('#TomorrowBrand h1',{
+    gsap.from(`${TomorrowBrand} h1`,{
         y:20,
         opacity:0,
         duration:1,
         scrollTrigger:{
-            trigger:'#text-black',
+            trigger:textBlack,
             
             start:'top 80%',
         }
     })
-    gsap.to('#text-black',{
+    gsap.to(textBlack,{
         width:'100%',
         
         scrollTrigger:{
-            trigger:'#text-black',
+            trigger:textBlack,
             
             scrub:1,
             start:'top 60%',
@@ -178,7 +144,22 @@ const page2=()=>{
         }
     })
 }
+const events=()=>{
+    const divs=document.querySelectorAll('#cardContainer div')
+    divs.forEach(div=>{
+        // console.log(div.children[1])
+        div.addEventListener('mouseenter',()=>{
+
+            div.children[1].load()
+            div.children[1].play()
+        })
+        
+    })
+}
+
 navberDesign()
 // locomotive()
 curser()
-page2()
+page2('.page2','#text-black','#TomorrowBrand')
+page2('.page5','#text-black2','#partner')
+events()
